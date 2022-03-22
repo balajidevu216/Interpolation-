bdbd0918041c98bb84d31c0687cd4bc6d9af9912
Balaji <balajidevu216@gmail.com>
2022-03-22 19:54:00 +0530

    today learn binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IchildComponent } from './ichild/ichild.component';
+import { BindingsComponent } from './bindings/bindings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IchildComponent
+    IchildComponent,
+    BindingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
